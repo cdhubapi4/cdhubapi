f75218b15b98635c4dbb6ad230c5cf744ad07f37
minsekim1 <tkarnrwl7862@naver.com>
2025-01-18 12:26:30 +0900

    version upgrade

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import fetch from "@utils/fetch";
+import getPackajsonVersion from "@utils/getPackajsonVersion";
 import axios from "axios";
 import Link from "next/link";
 
 const IndexPage = () => {
   const BASE_URL = `https://cdhubapi2.vercel.app/api`;
+  const version = getPackajsonVersion()
   return (
     <div>
-      <h1>Hello Next.js 3👋</h1>
+      <h1>Hello Next.js 4 {version}👋</h1>
       <button onClick={() => axios.get(`${BASE_URL}/common/ping`)}>CHECK PING </button>
       <button onClick={() => axios.get(`${BASE_URL}/common/dbcheck`)}>CHECK DB Connection </button>
       <button onClick={() => axios.get(`${BASE_URL}/user?page=1&size=2`)}>GET USER </button>
